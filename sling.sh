29ded43f673600962e6dac60500449c1ce4ed735
mhlinder <mhlinder@gmail.com>
2013-11-19 18:15:51 -0500

    cleanup

--- sling.sh
+++ sling.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-notes=$HOME/Programming/Notes/
-dropbox=$HOME/Dropbox/Notes/
+# This script requires that the notes and dropbox directories are set up with
+# local git repos, cloned from a private Github repository.
+src=$HOME/Programming/Notes/
+dst=$HOME/Dropbox/Notes/
+if [ "$1" == "slang" ]; then
+    src=$HOME/Dropbox/Notes/
+    dst=$HOME/Programming/Notes/
+fi
 
 td=`date`
 echo $td
 
-cd notes
+cd $src
 git add .
 git commit -a -m "$td" --allow-empty
+git push origin master
 
-cd $dropbox
+cd $dst
 git add .
-git commit -a -m "$td" --allow-empty
+git pull origin master
