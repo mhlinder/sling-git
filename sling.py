b46e151b15e7a7580634c67723de0356a50fedd4
mhlinder <mhlinder@gmail.com>
2013-11-19 14:52:08 -0500

    structured; setup to loop through directories and do initial commit

--- sling.py
+++ sling.py
@@ -1,4 +1,6 @@
 import os
+import time, datetime
+from git import *
 
 # push_to = 1 if pushing from local to dropbox
 push_to = 1
@@ -7,15 +9,20 @@ txt_ext = '.txt'
 structure_dirs = ['index', 'archive', 'active']
 exts = ['.txt', '.otl']
 
-dropbox_dir = '/Users/henry/Dropbox/Notes/'
-local_dir = '/Users/henry/Programming/Notes/'
+dirs = {'dropbox_dir': '/Users/henry/Dropbox/Notes/', 'local_dir': '/Users/henry/Programming/Notes/'}
+
+# commit each directory first
+repo = Repo(dirs['local_dir'])
+ts = time.time()
+current_time = datetime.datetime.fromtimestamp(ts).strftime('%a %b %d %I:%M:%S %Y')
+
 
 if push_to:
-    notes_dir = local_dir
-    dst_dir = dropbox_dir
+    notes_dir = dirs['local_dir']
+    dst_dir = dirs['dropbox_dir']
 else:
-    notes_dir = dropbox_dir
-    dst_dir = local_dir
+    notes_dir = dirs['dropbox_dir']
+    dst_dir = dirs['local_dir']
 
 mv_list = [] # (from, to)
 for dirname, dirnames, filenames in os.walk(notes_dir):
