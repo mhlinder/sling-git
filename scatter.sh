c7af2f47eba5490877ca0900c5a338b6063f5f1c
mhlinder <mhlinder@gmail.com>
2013-11-19 21:59:23 -0500

    scattering to github Tue Nov 19 21:59:15 EST 2013

--- scatter.sh
+++ scatter.sh
@@ -7,13 +7,15 @@ main=$HOME/Programming
 td=`date`
 echo $td
 
-# find all non-vim repos
-for repo in `find $main -name .git | awk '!/\.vim/ { print }'`
-do
-    cd ${repo%\/\.git}
-    git commit -a -m "scattering to github $td" --allow-empty
-    git push origin master
-    git push origin develop
-done
+if [ "$1" != "scetter" ]; then
+    # find all non-vim repos
+    for repo in `find $main -name .git | awk '!/\.vim/ { print }'`
+    do
+        cd ${repo%\/\.git}
+        git commit -a -m "scattering to github $td" --allow-empty
+        git push origin master
+        git push origin develop
+    done
+fi
 
 cd $START
